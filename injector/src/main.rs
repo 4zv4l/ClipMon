1238f0f9423af711de26cd15211c1427a6c2463b
azz <4zv4l@protonmail.com>
2023-07-04 14:55:17 +0200

    add log to stdout as well

--- injector/src/main.rs
+++ injector/src/main.rs
@@ -1,6 +1,8 @@
 #![cfg(target_os = "windows")]
 
 use tracing_appender::non_blocking::WorkerGuard;
+use tracing_subscriber::fmt;
+use tracing_subscriber::prelude::*;
 
 use dll_syringe::{Syringe, process::OwnedProcess};
 use std::process::Command;
@@ -17,9 +19,13 @@ fn start_process(proc: &str) -> Result<std::process::Child, std::io::Error> {
 
 fn setup_logging() -> WorkerGuard {
     let file_appender = tracing_appender::rolling::daily("", "clipmon.log");
-    let (non_blocking, _guard) = tracing_appender::non_blocking(file_appender);
-    tracing_subscriber::fmt().with_writer(non_blocking).init();
-    _guard
+    let (file_writer, guard) = tracing_appender::non_blocking(file_appender);
+    tracing::subscriber::set_global_default(
+        fmt::Subscriber::builder()
+            .finish()
+            .with(fmt::Layer::default().with_writer(file_writer))
+    ).expect("Unable to set global tracing subscriber");
+    guard
 }
 
 fn main() {
@@ -27,10 +33,12 @@ fn main() {
     const DLL: &str = "clipmon.dll";
 
     let _guard = setup_logging();
-    
+
     tracing::info!("Creating {TARGET}");
-    let proc = start_process(TARGET).unwrap();
-    tracing::info!("{TARGET} is Created => {}", proc.id());
+    match start_process(TARGET) {
+        Ok(proc) => tracing::info!("{TARGET} is Created => {}", proc.id()),
+        Err(e) => {tracing::error!("{e}");return}
+    };
     match OwnedProcess::find_first_by_name(TARGET) {
         Some(process) => {
             tracing::info!("found {:?}", process);
