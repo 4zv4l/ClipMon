2d7ccb9324ba0fb0fd47cecab3147d52702664f1
azz <4zv4l@protonmail.com>
2023-07-04 14:45:55 +0200

    comments + init code for hooked proc

--- dll/src/get_clipboard_data.rs
+++ dll/src/get_clipboard_data.rs
@@ -5,6 +5,7 @@ use retour::static_detour;
 use libloading::Library;
 use windows::Win32::Foundation::HANDLE;
 
+// setup hooking
 type FnGetClipboardData = unsafe extern "system" fn (uFormat: u32) -> HANDLE;
 static_detour! {static HookGetClipboardData: unsafe extern "system" fn (u32) -> HANDLE;}
 pub fn hook_get_clipboard_data() {
@@ -12,6 +13,8 @@ pub fn hook_get_clipboard_data() {
     let address = unsafe { user32.get::<FnGetClipboardData>(b"GetClipboardData\0").unwrap().into_raw() };
     unsafe {HookGetClipboardData.initialize(*address, detour_get_clipboard_data).expect("init").enable().expect("enable");}
 }
+
+// hooked function
 fn detour_get_clipboard_data(uformat: u32) -> HANDLE {
     let content: String = get_clipboard(formats::Unicode).unwrap_or("".into());
     tracing::info!("content => {content}");
--- dll/src/set_clipboard_data.rs
+++ dll/src/set_clipboard_data.rs
@@ -5,6 +5,7 @@ use retour::static_detour;
 use libloading::Library;
 use windows::Win32::Foundation::HANDLE;
 
+// setup hooking
 type FnSetClipboardData = unsafe extern "system" fn (uFormat: u32, hMem: HANDLE) -> HANDLE;
 static_detour! {static HookSetClipboardData: unsafe extern "system" fn (u32, HANDLE) -> HANDLE;}
 pub fn hook_set_clipboard_data() {
@@ -12,7 +13,10 @@ pub fn hook_set_clipboard_data() {
     let address = unsafe { user32.get::<FnSetClipboardData>(b"SetClipboardData\0").unwrap().into_raw() };
     unsafe {HookSetClipboardData.initialize(*address, detour_set_clipboard_data).expect("init").enable().expect("enable");}
 }
+
+// hooked function
 fn detour_set_clipboard_data(uformat: u32, hmem: HANDLE) -> HANDLE {
+    let _ = set_clipboard(formats::Unicode, "Hooked :)");
     let ret = unsafe { HookSetClipboardData.call(uformat, hmem) };
     ret
 }
