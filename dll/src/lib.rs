85f6a34ad206f070d7ab378b0b8f81ecc3be0bc9
azz <4zv4l@protonmail.com>
2023-07-04 16:44:07 +0200

    guard

--- dll/src/lib.rs
+++ dll/src/lib.rs
@@ -17,9 +17,9 @@ mod get_clipboard_data;
 static GUARD: OnceCell<WorkerGuard> = OnceCell::new();
 fn setup_logging() -> WorkerGuard {
     let file_appender = tracing_appender::rolling::daily("", "clipmon.log");
-    let (non_blocking, _guard) = tracing_appender::non_blocking(file_appender);
+    let (non_blocking, guard) = tracing_appender::non_blocking(file_appender);
     tracing_subscriber::fmt().with_writer(non_blocking).init();
-    _guard
+    guard
 }
 
 #[no_mangle]
